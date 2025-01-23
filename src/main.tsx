b3246fbe05e51bfa92609b0b4debec2fc3f4abcc
solmee <kii5541@naver.com>
2025-01-24 02:12:01 +0900

    chore: add tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { YDSWrapper } from "@yourssu/design-system-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <YDSWrapper>
-      <App />
-    </YDSWrapper>
+    <QueryClientProvider client={queryClient}>
+      <YDSWrapper>
+        <App />
+      </YDSWrapper>
+    </QueryClientProvider>
   </StrictMode>
 );
