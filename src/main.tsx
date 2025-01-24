e453ffc04fa3eb83e4a02bca441421a6ec6489e2
owl1753 <bighammer1753@gmail.com>
2025-01-24 16:38:45 +0900

    chore: vite 버전 업 및 react-router 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import {BrowserRouter, Route, Routes} from "react-router";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <YDSWrapper>
-        <App />
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<App />}></Route>
+              </Routes>
+          </BrowserRouter>
       </YDSWrapper>
     </QueryClientProvider>
   </StrictMode>
