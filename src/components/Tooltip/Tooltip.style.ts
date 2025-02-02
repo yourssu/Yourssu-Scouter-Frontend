225394a1a98065884d207e9fb9f665a741349c84
solmee <kii5541@naver.com>
2025-02-02 17:13:11 +0900

    feat: tooltip 컴포넌트 제작

---
+++ src/components/Tooltip/Tooltip.style.ts
@@ -0,0 +1,94 @@
+import styled, { css } from "styled-components";
+
+export const TooltipWrapper = styled.div`
+  position: relative;
+  display: inline-flex;
+  width: fit-content;
+  height: fit-content;
+`;
+
+interface TooltipContentProps {
+  $position?: "top" | "bottom" | "left" | "right";
+  $offset?: number;
+}
+
+export const TooltipContent = styled.div<TooltipContentProps>`
+  position: absolute;
+  white-space: nowrap;
+  z-index: 1;
+  padding: 8px 12px;
+  background: #373a43;
+  border-radius: ${({ theme }) => theme.semantic.radius.xs}px;
+  color: ${({ theme }) => theme.semantic.color.textBasicWhite};
+  ${({ theme }) => theme.typo.C1_Rg_13};
+
+  ${({ $position = "top", $offset = 10 }) => {
+    switch ($position) {
+      case "bottom":
+        return css`
+          top: calc(100% + ${$offset}px);
+          left: 50%;
+          transform: translateX(-50%);
+
+          &::after {
+            top: -8px;
+            bottom: auto;
+            border-top: none;
+            border-bottom: 8px solid #373a43;
+          }
+        `;
+      case "left":
+        return css`
+          right: calc(100% + ${$offset}px);
+          top: 50%;
+          transform: translateY(-50%);
+          margin-bottom: 0;
+
+          &::after {
+            left: auto;
+            right: -8px;
+            top: 50%;
+            transform: translateY(-50%);
+            border-left: 8px solid #373a43;
+            border-top: 8px solid transparent;
+            border-bottom: 8px solid transparent;
+            border-right: none;
+          }
+        `;
+      case "right":
+        return css`
+          left: calc(100% + ${$offset}px);
+          top: 50%;
+          transform: translateY(-50%);
+          margin-bottom: 0;
+
+          &::after {
+            left: -8px;
+            top: 50%;
+            transform: translateY(-50%);
+            border-right: 8px solid #373a43;
+            border-top: 8px solid transparent;
+            border-bottom: 8px solid transparent;
+            border-left: none;
+          }
+        `;
+      default:
+        return css`
+          bottom: calc(100% + ${$offset}px);
+          left: 50%;
+          transform: translateX(-50%);
+
+          &::after {
+            content: "";
+            position: absolute;
+            bottom: -8px;
+            left: 50%;
+            transform: translateX(-50%);
+            border-left: 8px solid transparent;
+            border-right: 8px solid transparent;
+            border-top: 8px solid #373a43;
+          }
+        `;
+    }
+  }}
+`;
