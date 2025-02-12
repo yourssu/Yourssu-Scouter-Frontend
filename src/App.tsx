300d444686318ba65a5683e7def6fb7bb688ce3c
solmee <kii5541@naver.com>
2025-02-12 16:50:54 +0900

    chore: test 페이지 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import {Route, Routes} from "react-router";
 import Main from "@/components/Main/Main.tsx";
-import {Members} from "@/pages/Members/Members.tsx";
-import { Test } from "./pages/Test";
+import { Members } from "@/pages/Members/Members.tsx";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
-      <Routes>
-          <Route path="test" element={<Test />} />
-        <Route path="*" element={<Main />}>
-          <Route index element={<>응애</>} />
-          <Route path="members" element={<Members />} />
-        </Route>
-      </Routes>
-  )
+    <Routes>
+      <Route path="*" element={<Main />}>
+        <Route index element={<>응애</>} />
+        <Route path="members" element={<Members />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
