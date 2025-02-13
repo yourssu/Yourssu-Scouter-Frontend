ee25d808aa1c51bb7342d1300706ab6127564aad
solmee <kii5541@naver.com>
2025-02-13 15:04:43 +0900

    feat: auth 인증 인터셉터가 포함된 API 인스턴스 및 토큰 서비스 구현
    - ky 인스턴스에 인증 토큰 처리 추가
    - 토큰 갱신 및 저장 서비스 구현
    - 관련 타입 정의

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,6 +1,49 @@
 import { API_CONFIG } from "@/constants/config";
-import ky from "ky";
+import ky, { type BeforeRetryHook, HTTPError } from "ky";
+import { authService } from "./auth.service";
+import { tokenService } from "./token.service";
+
+const DEFAULT_API_RETRY_LIMIT = 2;
+
+const handleTokenRefresh: BeforeRetryHook = async ({ error, retryCount }) => {
+  const httpError = error as HTTPError;
+
+  if (httpError.response.status !== 401) {
+    return ky.stop;
+  }
+
+  if (retryCount === DEFAULT_API_RETRY_LIMIT - 1) {
+    authService.logout();
+    return ky.stop;
+  }
+
+  try {
+    const refreshToken = tokenService.getRefreshToken();
+    if (!refreshToken) {
+      throw new Error("refresh Token이 없음");
+    }
+    await authService.refreshToken(refreshToken);
+  } catch (error) {
+    console.error("Token refresh 실패, 로그아웃", error);
+    authService.logout();
+    return ky.stop;
+  }
+};
+
+const setAuthHeader = (request: Request) => {
+  const accessToken = tokenService.getAccessToken();
+  if (accessToken) {
+    request.headers.set("Authorization", `${accessToken}`);
+  }
+};
 
 export const api = ky.create({
   prefixUrl: API_CONFIG.BASE_URL,
+  retry: {
+    limit: DEFAULT_API_RETRY_LIMIT,
+  },
+  hooks: {
+    beforeRequest: [setAuthHeader],
+    beforeRetry: [handleTokenRefresh],
+  },
 });
