7f6dcf12f2acbf9d371c83e1be07a490c046103b
solmee <kii5541@naver.com>
2025-03-09 08:45:45 +0900

    feat: StateButton 컴포넌트에 아이콘 커스텀 기능 추가
    - 아이콘을 props로 받을 수 있도록 수정
    - 기본값은 IcArrowsChevronUpLine 으로 유지

--- src/components/StateButton/SemesterStateButton.tsx
+++ src/components/StateButton/SemesterStateButton.tsx
@@ -1,25 +1,27 @@
-import {StateButton} from "./StateButton";
-import {useGetSemesters} from "@/hooks/useGetSemesters.ts";
+import { useGetSemesters } from "@/hooks/useGetSemesters.ts";
+import { IcArrowsChevronDownLine } from "@yourssu/design-system-react";
+import { StateButton } from "./StateButton";
 
 export const SemesterStateButton = ({
-                                        selectedValue,
-                                        onStateChange,
-                                        size = "small",
-                                    }: {
-    selectedValue: string;
-    onStateChange: (value: string) => void;
-    size?: "small" | "medium";
+  selectedValue,
+  onStateChange,
+  size = "small",
+}: {
+  selectedValue: string;
+  onStateChange: (value: string) => void;
+  size?: "small" | "medium";
 }) => {
-    const {data: semesters} = useGetSemesters();
-    const options = semesters.map(semester => ({label: semester.semester}));
+  const { data: semesters } = useGetSemesters();
+  const options = semesters.map((semester) => ({ label: semester.semester }));
 
-    return (
-        <StateButton
-            options={options}
-            selectedValue={selectedValue}
-            onSelect={onStateChange}
-            variant="outlined"
-            size={size}
-        />
-    );
+  return (
+    <StateButton
+      options={options}
+      selectedValue={selectedValue}
+      onSelect={onStateChange}
+      variant="outlined"
+      size={size}
+      icon={<IcArrowsChevronDownLine />}
+    />
+  );
 };
