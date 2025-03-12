faa257a37f3c4d2374f845ed3e81d2b323792e9b
solmee <kii5541@naver.com>
2025-03-12 15:41:57 +0900

    feat: 메일 전송 페이지 생성 및 라우터에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { GoogleCallback } from "@/components/Auth/GoogleCallback";
 import Main from "@/components/Main/Main.tsx";
 import { Members } from "@/pages/Members/Members.tsx";
 import { Route, Routes } from "react-router";
+import { SendMail } from "./pages/SendMail/SendMail";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="*" element={<Main />}>
         <Route index element={<>응애</>} />
         <Route path="members" element={<Members />} />
+        <Route path="send-mail" element={<SendMail />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/SendMail/SendMail.tsx
@@ -0,0 +1,1 @@
+export const SendMail = () => {return<div></div>}
