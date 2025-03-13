f3165449499b054769b773c0a95916423d66cb9e
owl1753 <bighammer1753@gmail.com>
2025-03-13 14:02:06 +0900

    refactor: 전역 Suspense 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { Members } from '@/pages/Members/Members.tsx';
 import { Route, Routes } from 'react-router';
 import ScouterErrorBoundary from '@/components/ScouterErrorBoundary.tsx';
 import { Applicants } from '@/pages/Applicants/Applicants.tsx';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <ScouterErrorBoundary>
-      <Routes>
-        <Route path="/oauth/callback/google" element={<GoogleCallback />} />
-        <Route path="*" element={<Main />}>
-          <Route index element={<>응애</>} />
-          <Route path="members" element={<Members />} />
-          <Route path="recruiting" element={<Applicants />} />
-        </Route>
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/oauth/callback/google" element={<GoogleCallback />} />
+          <Route path="*" element={<Main />}>
+            <Route index element={<>응애</>} />
+            <Route path="members" element={<Members />} />
+            <Route path="recruiting" element={<Applicants />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </ScouterErrorBoundary>
   );
 }
