8d80fc771b51ddced5a15404b65c9915c9e67dc4
owl1753 <bighammer1753@gmail.com>
2025-04-01 21:34:01 +0900

    fix: queryKey 순환참조 제거

--- src/pages/Applicants/components/ApplicantTableFallback/ApplicantTableFallback.tsx
+++ src/pages/Applicants/components/ApplicantTableFallback/ApplicantTableFallback.tsx
@@ -1,19 +1,12 @@
 import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import Table from '@/components/Table/Table.tsx';
-import { useQueryClient } from '@tanstack/react-query';
-import { applicantOptions } from '@/query/applicant/options.ts';
 import { useApplicantColumns } from '@/query/applicant/hooks/useApplicantColumns.tsx';
 
 const ApplicantTableFallback = () => {
-  const queryClient = useQueryClient();
-
-  const data =
-    queryClient.getQueryData(applicantOptions().queryKey) ?? ([] as const);
-
   const columns = useApplicantColumns();
 
   const table = useReactTable({
-    data,
+    data: [],
     columns,
     getCoreRowModel: getCoreRowModel(),
     enableColumnResizing: true,
--- src/pages/Members/components/MemberTableFallback/MemberTableFallback.tsx
+++ src/pages/Members/components/MemberTableFallback/MemberTableFallback.tsx
@@ -1,8 +1,6 @@
 import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import Table from '@/components/Table/Table.tsx';
 import { MemberState } from '@/query/member/schema.ts';
-import { useQueryClient } from '@tanstack/react-query';
-import { memberOptions } from '@/query/member/options.ts';
 import { useMemberColumns } from '@/query/member/hooks/useMemberColumns.tsx';
 
 interface MemberTableFallbackProps {
@@ -10,16 +8,10 @@ interface MemberTableFallbackProps {
 }
 
 const MemberTableFallback = ({ state }: MemberTableFallbackProps) => {
-  const queryClient = useQueryClient();
-  const data =
-    queryClient.getQueryData(memberOptions(state).queryKey) || ([] as const);
-
-  console.log('tableData', data);
-
   const columns = useMemberColumns(state);
 
   const table = useReactTable({
-    data,
+    data: [],
     columns,
     getCoreRowModel: getCoreRowModel(),
     enableColumnResizing: true,
