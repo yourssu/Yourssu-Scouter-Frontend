90dc045b0f014377c2d2ba49c5be5e3828293a86
심민성 <75308777+owl1753@users.noreply.github.com>
2025-04-11 21:17:51 +0900

    feat: 정보 수정 스낵바 추가 (#22)
    * refactor: 주석 제거
    
    * chore: lock 파일 추가

--- src/pages/Members/components/MemberTable/MemberTable.tsx
+++ src/pages/Members/components/MemberTable/MemberTable.tsx
@@ -5,6 +5,7 @@ import { useInvalidateMembers } from '@/query/member/hooks/useInvalidateMembers.
 import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
 import { memberOptions } from '@/query/member/options.ts';
 import { patchMember } from '@/query/member/mutations/patchMember.ts';
+import { useSnackbar } from '@yourssu/design-system-react';
 import {
   PatchMemberHandler,
   useMemberColumns,
@@ -17,9 +18,34 @@ interface MemberTableProps {
 
 const MemberTable = ({ state, search }: MemberTableProps) => {
   const invalidateMembers = useInvalidateMembers(state);
+  const { snackbar } = useSnackbar();
+
   const { mutate: patchMemberMutate } = useMutation({
     mutationFn: patchMember,
-    onSuccess: invalidateMembers,
+    onSuccess: async (_, { memberId }) => {
+      await invalidateMembers();
+
+      const member = data.find((d) => d.memberId === memberId);
+
+      if (!member) return;
+
+      snackbar({
+        type: 'info',
+        width: '400px',
+        message: `${member.nickname}님의 정보가 변경되었습니다.`,
+        duration: 3000,
+        position: 'center',
+      });
+    },
+    onError: () => {
+      snackbar({
+        type: 'error',
+        width: '400px',
+        message: '입력 형식이 올바르지 않습니다.',
+        duration: 3000,
+        position: 'center',
+      });
+    },
   });
 
   const { data } = useSuspenseQuery(memberOptions(state, { search }));
