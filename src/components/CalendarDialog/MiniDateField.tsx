dc57b4ea11b19707f8d3495d7fdbe8d06329e9f5
wwwlnyy <wwwlnyy@gmail.com>
2025-07-17 10:40:16 +0900

    fix: MiniDateField에서 시간 표시 방법 수정

--- src/components/CalendarDialog/MiniDateField.tsx
+++ src/components/CalendarDialog/MiniDateField.tsx
@@ -17,6 +17,22 @@ export const MiniDateField = ({
 }: DateFieldProps ) => {
     const IconComponent = iconMap[icon];
 
+    if (icon == 'IcClockLine' && date) {
+        const [hours, minutes] = date.split(':');
+        let hoursNum = parseInt(hours, 10);
+        if (hoursNum < 12) {
+            hoursNum += 12;
+            date = `오전 ${hoursNum}:${minutes}`;
+        } else {
+            hoursNum -= 12;
+            date = `오후 ${hoursNum}:${minutes}`;
+        }
+    }
+    else if (icon == 'IcClockLine' && !date)
+        date = '오전 12:00';
+    else if (icon == 'IcCalendarLine' && !date)
+        date = '날짜 선택 안됨';
+
     return (
         <MiniDateFieldContainer $width={366} $height={48}>
             {/* 아이콘 색깔 바꾸기 */}
