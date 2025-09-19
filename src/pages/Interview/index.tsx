9beec45bd5afb5ae904ac57739199350cf3f829c
fecapark <fecapark@gmail.com>
2025-09-19 10:03:05 +0900

    feat: 면접 일정 페이지 라우트 추가

---
+++ src/pages/Interview/index.tsx
@@ -0,0 +1,9 @@
+import { StyledContainer, StyledTitle } from '@/styles/pages/table';
+
+export const InterviewPage = () => {
+  return (
+    <StyledContainer>
+      <StyledTitle>면접 일정 관리</StyledTitle>
+    </StyledContainer>
+  );
+};
