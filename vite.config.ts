c2c651a335ebef0441688e5a708a6af711c69d00
Sanghyeok Park <fecapark@gmail.com>
2025-09-21 04:09:50 +0900

    chore: 환경변수로 옵셔널하게 vite HMR polling 방식을 트리거해요. (#40)
    * package: @types/node
    
    * feat: 프로젝트 환경변수 타입 추가
    
    * chore: 환경변수에 따라 HMR에 polling을 사용할 수 있도록 옵션 제공

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const useViteHMRPolling = (env: Record<string, string>) => {
+  if (env.VITE_USE_POLLING !== 'true') {
+    return false;
+  }
+  // console orange color
+  console.warn(
+    '\x1b[33m[WARN] Vite HMR에 Polling을 사용하도록 변경했어요. CPU 사용에 유의해주세요.\x1b[0m',
+  );
+  return true;
+};
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      watch: {
+        usePolling: useViteHMRPolling(env),
+      },
+    },
+  };
 });
