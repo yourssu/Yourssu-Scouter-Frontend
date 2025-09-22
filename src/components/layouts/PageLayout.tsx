f4f37b2c27e2a1bdae9c28034d5fa311df7cac08
fecapark <fecapark@gmail.com>
2025-09-23 00:09:42 +0900

    feat: PageLayout 컴포넌트 구현

---
+++ src/components/layouts/PageLayout.tsx
@@ -0,0 +1,12 @@
+interface PageLayoutProps {
+  title: string;
+}
+
+export const PageLayout = ({ children, title }: React.PropsWithChildren<PageLayoutProps>) => {
+  return (
+    <div className="flex size-full flex-col">
+      <h1 className="typo-h3_sb_32 m-10 mb-4">{title}</h1>
+      {children}
+    </div>
+  );
+};
