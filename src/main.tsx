9ccb647c86eb737c5e4305a1629c7ff9517c2047
Sanghyeok Park <fecapark@gmail.com>
2025-09-22 17:01:35 +0900

    프로젝트에 tailwindcss를 셋업해요 (#41)
    * package: tailwind, @tailwindcss/vite
    
    * package: upgrade vite, @vitejs/plugin-react-swc
    
    * chore: vite 테일윈드 설정 추가
    
    * feat: tailwindcss를 index.css에 적용
    
    - base 레이어 별도의 css 파일로 분리
    - css 파일들을 styles/ 하위에서 관리하도록 변경
    
    * package: prettier-plugin-tailwindcss
    
    * chore: tailwind prettier 에디터 세팅 적용
    
    * package: clsx, tailwind-merge
    
    * feat: cn 유틸리티 함수 구현
    
    * chore: tailwind 관련 유틸리티 함수들 에디터 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
-import './index.css';
 import App from '@/App.tsx';
+
+import './styles/index.css';
 
 const queryClient = new QueryClient();
 
