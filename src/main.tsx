6cbd21ba98c339b2a3cd3317b1933ed292f881d6
fecapark <fecapark@gmail.com>
2025-09-22 23:56:31 +0900

    fix: YDSWrapper를 YDSThemeProvider로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { SnackbarProvider, YDSWrapper } from '@yourssu/design-system-react';
+import { SnackbarProvider, YDSThemeProvider } from '@yourssu/design-system-react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
@@ -14,13 +14,13 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <YDSWrapper>
+      <YDSThemeProvider>
         <SnackbarProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
         </SnackbarProvider>
-      </YDSWrapper>
+      </YDSThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
