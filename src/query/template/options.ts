065c3516be3e61bd957396634df7409b19f73ff3
wwwlnyy <wwwlnyy@gmail.com>
2025-10-10 17:26:16 +0900

    feat: templateOptions에 템플릿 상세 조회 로직 추가

--- src/query/template/options.ts
+++ src/query/template/options.ts
@@ -1,15 +1,30 @@
 import { queryOptions } from '@tanstack/react-query';
 
 import { api } from '@/apis/api.ts';
-import { TemplateListSchema } from '@/query/template/schema';
+import { TemplateListSchema, TemplateSchema } from '@/query/template/schema';
 
-export const templateOptions = () => {
-  return queryOptions({
-    queryKey: ['templates'],
-    queryFn: async () => {
-      const data = await api.get('api/mails/templates').json();
-      return TemplateListSchema.parse(data);
-    },
-    staleTime: Infinity,
-  });
+const templateKeys = {
+  all: ['templates'] as const,
+  detail: (id: number) => [...templateKeys.all, id] as const,
+};
+
+export const templateOptions = {
+  all: () =>
+    queryOptions({
+      queryKey: ['templates'],
+      queryFn: async () => {
+        const data = await api.get('api/mails/templates').json();
+        return TemplateListSchema.parse(data);
+      },
+      staleTime: Infinity,
+    }),
+  detail: (templateId: number) =>
+    queryOptions({
+      queryKey: ['templates', templateId],
+      queryFn: async () => {
+        const data = await api.get(`api/mails/templates/${templateId}`).json();
+        return TemplateSchema.parse(data);
+      },
+      staleTime: Infinity,
+    }),
 };
