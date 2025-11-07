c7f399ad7ce2bbd01509849b692585377363f522
wwwlnyy <wwwlnyy@gmail.com>
2025-11-08 05:37:42 +0900

    refactor: 템플릿 조회/생성 양식에 맞게 변경

--- src/query/template/options.ts
+++ src/query/template/options.ts
@@ -1,7 +1,17 @@
 import { queryOptions } from '@tanstack/react-query';
 
 import { api } from '@/apis/api.ts';
-import { TemplateListSchema, TemplateSchema } from '@/query/template/schema';
+import { BaseTemplateSchema, TemplateListResponseSchema } from '@/query/template/schema';
+import { defaultVariables, VariableType } from '@/types/editor';
+import { formatTemplates } from '@/utils/date';
+import { transformBodyHtmlToContent } from '@/utils/transformTemplate.ts';
+
+const TypeMap: Record<string, VariableType> = {
+  PERSON: '사람',
+  DATE: '날짜',
+  LINK: '링크',
+  TEXT: '텍스트',
+};
 
 const templateKeys = {
   all: ['templates'] as const,
@@ -14,7 +24,13 @@ export const templateOptions = {
       queryKey: ['templates'],
       queryFn: async () => {
         const data = await api.get('api/mails/templates').json();
-        return TemplateListSchema.parse(data);
+        const response = TemplateListResponseSchema.parse(data).content;
+        const templateData = response.map((template) => ({
+          id: template.id,
+          title: template.title,
+          date: formatTemplates['2025.01.01'](template.updatedAt),
+        }));
+        return templateData;
       },
       staleTime: Infinity,
     }),
@@ -23,7 +39,29 @@ export const templateOptions = {
       queryKey: ['templates', templateId],
       queryFn: async () => {
         const data = await api.get(`api/mails/templates/${templateId}`).json();
-        return TemplateSchema.parse(data);
+        const response = BaseTemplateSchema.parse(data);
+
+        const fixedVariables = response.variables.map((variable) => {
+          if (!variable.type) {
+            return defaultVariables.find((v) => v.id === variable.key)!;
+          }
+          return {
+            id: variable.key,
+            name: variable.displayName,
+            type: TypeMap[variable.type],
+            isFixed: false,
+            differentForEachPerson: variable.perRecipient,
+          };
+        });
+
+        const templateDetail = {
+          id: response.id,
+          title: response.title,
+          content: transformBodyHtmlToContent(response.bodyHtml, fixedVariables),
+          variables: fixedVariables,
+          date: formatTemplates['2025.01.01'](response.updatedAt),
+        };
+        return templateDetail;
       },
       staleTime: Infinity,
     }),
