5d45269baa728c7bb981a92dc001fe42478e6362
wwwlnyy <wwwlnyy@gmail.com>
2025-11-08 05:37:01 +0900

    feat: 백-프론트 응답 양식 맞추는 보조 함수 추가

--- src/utils/transformTemplate.ts
+++ src/utils/transformTemplate.ts
@@ -1,3 +1,4 @@
+import { getChipType } from '@/components/VariableChip/utils';
 import { Variable } from '@/types/editor.ts';
 
 const typeMap: {
@@ -9,7 +10,7 @@ const typeMap: {
   텍스트: 'TEXT',
 };
 
-export const transformBodyHtml = (content: string) => {
+export const transformContentToBodyHtml = (content: string) => {
   const regex = /<span[^>]*data-key="([^"]*)"[^>]*><\/span>/g;
 
   return content.replace(regex, (_match, key) => {
@@ -17,10 +18,25 @@ export const transformBodyHtml = (content: string) => {
   });
 };
 
+export const transformBodyHtmlToContent = (bodyHtml: string, variables: Variable[]) => {
+  const regex = /{{(.*?)}}/g;
+
+  return bodyHtml.replace(regex, (match, key) => {
+    const variable = variables.find((v) => v.id === key);
+
+    if (!variable) {
+      return match;
+    }
+
+    return `<span data-key="${variable.id}" data-type="${getChipType(variable.type)}" data-label="${variable.name}" data-variable-chip=""></span>`;
+  });
+};
+
 export const transformVariables = (variables: Variable[]) => {
   return variables.map((variable) => {
     const baseVariable = {
       key: variable.id,
+      type: variable.isFixed ? null : typeMap[variable.type],
       displayName: variable.name,
       perRecipient: variable.differentForEachPerson,
     };
