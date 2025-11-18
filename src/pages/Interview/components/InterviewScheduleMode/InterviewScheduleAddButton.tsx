a571d4cf38895168f00c2272981f32b0af3b1bf0
fecapark <fecapark@gmail.com>
2025-11-18 18:07:52 +0900

    feat: 면접일정 모드 -> 희망일정 모드로 넘어가는 기능 구현

--- src/pages/Interview/components/InterviewScheduleMode/InterviewScheduleAddButton.tsx
+++ src/pages/Interview/components/InterviewScheduleMode/InterviewScheduleAddButton.tsx
@@ -1,9 +1,14 @@
 import { IcPlusLine } from '@yourssu/design-system-react';
 
+import { useInterviewCalendarModeContext } from '@/pages/Interview/context';
+
 export const InterviewScheduleAddButton = () => {
+  const { setCalendarMode } = useInterviewCalendarModeContext();
+
   return (
     <button
       className="flex h-10 items-center justify-between gap-1 rounded-xl bg-gray-800 px-4 text-sm text-white hover:bg-gray-700"
+      onClick={() => setCalendarMode('희망일정')}
       type="button"
     >
       면접 일정 추가하기
