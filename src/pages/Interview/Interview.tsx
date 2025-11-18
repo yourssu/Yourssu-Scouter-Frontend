4ae989ea51d14ca0c4db824dd0ce37446d388164
fecapark <fecapark@gmail.com>
2025-11-18 18:00:52 +0900

    feat: 면접 캘린더 뷰 모드를 관리할 컨텍스트 구현

--- src/pages/Interview/Interview.tsx
+++ src/pages/Interview/Interview.tsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+import { SwitchCase } from 'react-simplikit';
+
 import { InterviewScheduleMode } from '@/pages/Interview/components/InterviewScheduleMode';
+import { InterviewCalendarModeContext } from '@/pages/Interview/context';
+import { CalendarModeType } from '@/pages/Interview/type';
 
 export const InterviewPage = () => {
-  return <InterviewScheduleMode />;
+  const [calendarMode, setCalendarMode] = useState<CalendarModeType>('면접일정');
+
+  return (
+    <InterviewCalendarModeContext.Provider value={{ calendarMode, setCalendarMode }}>
+      <SwitchCase
+        caseBy={{
+          면접일정: () => <InterviewScheduleMode />,
+          수동생성: () => <div />,
+          자동생성: () => <div />,
+          희망일정: () => <div />,
+        }}
+        value={calendarMode}
+      />
+    </InterviewCalendarModeContext.Provider>
+  );
 };
