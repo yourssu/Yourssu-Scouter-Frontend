48ab0a000aa0b09689d0d5c526554ef5bed7c81a
fecapark <fecapark@gmail.com>
2025-11-18 17:21:06 +0900

    refactor: 면접 일정 페이지 > 헤더를 공통 레이아웃으로 리팩토링

---
+++ src/pages/Interview/components/InterviewScheduleMode/InterviewScheduleAddButton.tsx
@@ -0,0 +1,13 @@
+import { IcPlusLine } from '@yourssu/design-system-react';
+
+export const InterviewScheduleAddButton = () => {
+  return (
+    <button
+      className="flex h-10 items-center justify-between gap-1 rounded-xl bg-gray-800 px-4 text-sm text-white hover:bg-gray-700"
+      type="button"
+    >
+      면접 일정 추가하기
+      <IcPlusLine className="size-3" />
+    </button>
+  );
+};
--- src/pages/Interview/components/InterviewScheduleMode/index.tsx
+++ src/pages/Interview/components/InterviewScheduleMode/index.tsx
@@ -2,8 +2,8 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { usePartFilter } from '@/hooks/usePartFilter';
 import { InterviewCalendar } from '@/pages/Interview/components/InterviewCalendar/InterviewCalendar';
-import { InterviewHeader } from '@/pages/Interview/components/InterviewHeader';
 import { InterviewPageLayout } from '@/pages/Interview/components/InterviewPageLayout';
+import { InterviewScheduleHeader } from '@/pages/Interview/components/InterviewScheduleMode/InterviewScheduleHeader';
 import { InterviewSidebar } from '@/pages/Interview/components/InterviewSidebar';
 import { useWeekIndicator } from '@/pages/Interview/hooks/useWeekIndicator';
 import { scheduleOptions } from '@/query/schedule/options';
@@ -18,13 +18,17 @@ export const InterviewScheduleMode = () => {
     <InterviewPageLayout
       slots={{
         header: (
-          <InterviewHeader
-            month={month}
-            onNextWeek={handleNextWeek}
-            onPartChange={onPartChange}
-            onPrevWeek={handlePrevWeek}
-            partName={partName}
-            week={week}
+          <InterviewScheduleHeader
+            indicator={{
+              month,
+              week,
+              onNextWeek: handleNextWeek,
+              onPrevWeek: handlePrevWeek,
+            }}
+            part={{
+              partName,
+              onPartChange,
+            }}
           />
         ),
         calendar: (
