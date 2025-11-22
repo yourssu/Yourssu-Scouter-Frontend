7956786fb546b190ca01c4f2d8e623ce3ddcf925
fecapark <fecapark@gmail.com>
2025-11-22 18:36:36 +0900

    feat: `희망일정` 모드 > 사이드바 일정 생성 버튼 구현

--- src/pages/Interview/components/AvailableTimesMode/AvailableTimesSidebar.tsx
+++ src/pages/Interview/components/AvailableTimesMode/AvailableTimesSidebar.tsx
@@ -1,3 +1,5 @@
+import { BoxButton } from '@yourssu/design-system-react';
+
 import { AvailableTimesSidebarDurationCard } from '@/pages/Interview/components/AvailableTimesMode/AvailableTimesSidebarDurationCard';
 import { InterviewSidebarLayout } from '@/pages/Interview/components/InterviewSidebarLayout';
 
@@ -7,6 +9,16 @@ export const AvailableTimesSidebar = () => {
       <InterviewSidebarLayout.CardList>
         <AvailableTimesSidebarDurationCard />
       </InterviewSidebarLayout.CardList>
+      <InterviewSidebarLayout.BottomArea>
+        <div className="grid grid-cols-2 gap-3">
+          <BoxButton className="w-full" size="xlarge" variant="filledSecondary">
+            일정 수동 생성
+          </BoxButton>
+          <BoxButton className="w-full" size="xlarge" variant="filledPrimary">
+            일정 자동 생성
+          </BoxButton>
+        </div>
+      </InterviewSidebarLayout.BottomArea>
     </InterviewSidebarLayout>
   );
 };
