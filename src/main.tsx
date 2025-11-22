53899f0a8b50e93d2fe9b54f2d9010bd67ed2c94
fecapark <fecapark@gmail.com>
2025-11-22 17:30:32 +0900

    feat: OverlayProvider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SnackbarProvider, YDSThemeProvider } from '@yourssu/design-system-react';
+import { OverlayProvider } from 'overlay-kit';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
@@ -17,11 +18,13 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <YDSThemeProvider>
-        <SnackbarProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </SnackbarProvider>
+        <OverlayProvider>
+          <SnackbarProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </SnackbarProvider>
+        </OverlayProvider>
       </YDSThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
