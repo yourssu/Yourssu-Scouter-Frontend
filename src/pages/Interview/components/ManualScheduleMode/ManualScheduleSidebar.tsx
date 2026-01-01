62908ecc17342e583ff0e2ef5d73569d3dca0d84
fecapark <fecapark@gmail.com>
2026-01-01 21:50:22 +0900

    feat: 수동생성모드 > 일정을 배정하지 않은 지원자가 있을 때 일정 저장하면 다이얼로그 띄우기

--- src/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebar.tsx
+++ src/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebar.tsx
@@ -1,5 +1,6 @@
 import { BoxButton } from '@yourssu/design-system-react';
 
+import { useAlertDialog } from '@/hooks/useAlertDialog';
 import { InterviewSidebarLayout } from '@/pages/Interview/components/InterviewSidebarLayout';
 import { ManualScheduleSidebarPartCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarPartCard';
 import { ManualScheduleSidebarProgressCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarProgressCard';
@@ -14,6 +15,19 @@ export const ManualScheduleSidebar = ({
   completedApplicants,
   totalApplicantCount,
 }: ManualScheduleSidebarProps) => {
+  const openAlertDialog = useAlertDialog();
+
+  const onSaveSchedule = () => {
+    if (completedApplicants.length < totalApplicantCount) {
+      openAlertDialog({
+        title: '지원자를 모두 배정해주세요',
+        content: '아직 면접 시간이 배정되지 않은 지원자가 있어요.',
+        primaryButtonText: '확인',
+      });
+      return;
+    }
+  };
+
   return (
     <InterviewSidebarLayout>
       <InterviewSidebarLayout.CardList>
@@ -24,7 +38,12 @@ export const ManualScheduleSidebar = ({
         />
       </InterviewSidebarLayout.CardList>
       <InterviewSidebarLayout.BottomArea>
-        <BoxButton className="w-full" size="xlarge" variant="filledPrimary">
+        <BoxButton
+          className="w-full"
+          onClick={onSaveSchedule}
+          size="xlarge"
+          variant="filledPrimary"
+        >
           시간표 저장하기
         </BoxButton>
       </InterviewSidebarLayout.BottomArea>
