c1746aae41c3785e634818ff5e00a55ca2d7e107
fecapark <fecapark@gmail.com>
2026-01-01 22:12:21 +0900

    feat: 수동생성모드 > 저장시 일정 생성 뮤테이션 연결

--- src/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebar.tsx
+++ src/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebar.tsx
@@ -1,13 +1,11 @@
-import { BoxButton } from '@yourssu/design-system-react';
-
-import { useAlertDialog } from '@/hooks/useAlertDialog';
 import { InterviewSidebarLayout } from '@/pages/Interview/components/InterviewSidebarLayout';
+import { ManualScheduleSaveButton } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSaveButton';
 import { ManualScheduleSidebarPartCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarPartCard';
 import { ManualScheduleSidebarProgressCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarProgressCard';
 import { Applicant } from '@/query/applicant/schema';
 
 interface ManualScheduleSidebarProps {
-  completedApplicants: Applicant[];
+  completedApplicants: [Date, Applicant][];
   totalApplicantCount: number;
 }
 
@@ -15,37 +13,20 @@ export const ManualScheduleSidebar = ({
   completedApplicants,
   totalApplicantCount,
 }: ManualScheduleSidebarProps) => {
-  const openAlertDialog = useAlertDialog();
-
-  const onSaveSchedule = () => {
-    if (completedApplicants.length < totalApplicantCount) {
-      openAlertDialog({
-        title: '지원자를 모두 배정해주세요',
-        content: '아직 면접 시간이 배정되지 않은 지원자가 있어요.',
-        primaryButtonText: '확인',
-      });
-      return;
-    }
-  };
-
   return (
     <InterviewSidebarLayout>
       <InterviewSidebarLayout.CardList>
         <ManualScheduleSidebarPartCard />
         <ManualScheduleSidebarProgressCard
-          completedApplicants={completedApplicants}
+          completedApplicants={completedApplicants.map(([, applicants]) => applicants)}
           totalApplicantCount={totalApplicantCount}
         />
       </InterviewSidebarLayout.CardList>
       <InterviewSidebarLayout.BottomArea>
-        <BoxButton
-          className="w-full"
-          onClick={onSaveSchedule}
-          size="xlarge"
-          variant="filledPrimary"
-        >
-          시간표 저장하기
-        </BoxButton>
+        <ManualScheduleSaveButton
+          completedApplicants={completedApplicants}
+          totalApplicantCount={totalApplicantCount}
+        />
       </InterviewSidebarLayout.BottomArea>
     </InterviewSidebarLayout>
   );
