c031c77741a62884ec577fecb58b1c1990e03134
fecapark <fecapark@gmail.com>
2026-01-01 17:40:25 +0900

    feat: 수동생성모드 진입가능 검증로직 추가

--- src/pages/Interview/components/AvailableTimesMode/AvailableTimesSidebar.tsx
+++ src/pages/Interview/components/AvailableTimesMode/AvailableTimesSidebar.tsx
@@ -1,11 +1,35 @@
 import { BoxButton } from '@yourssu/design-system-react';
 
+import { useAlertDialog } from '@/hooks/useAlertDialog';
 import { AvailableTimesSidebarDurationCard } from '@/pages/Interview/components/AvailableTimesMode/AvailableTimesSidebarDurationCard';
 import { InterviewSidebarLayout } from '@/pages/Interview/components/InterviewSidebarLayout';
-import { useInterviewCalendarModeContext } from '@/pages/Interview/context';
+import {
+  useInterviewCalendarModeContext,
+  useInterviewPartSelectionContext,
+} from '@/pages/Interview/context';
 
 export const AvailableTimesSidebar = () => {
+  const { partName } = useInterviewPartSelectionContext();
   const { setCalendarMode } = useInterviewCalendarModeContext();
+  const openAlertDialog = useAlertDialog();
+
+  const onManualScheduleModeButtonClick = () => {
+    if (!partName) {
+      openAlertDialog({
+        title: '파트를 선택해주세요',
+        content: (
+          <>
+            일정을 생성하려면 상단에서 파트를 먼저 선택해주세요.
+            <img src="/imgs/part-selection-guide.webp" />
+          </>
+        ),
+        primaryButtonText: '확인',
+      });
+      return;
+    }
+
+    setCalendarMode('수동생성');
+  };
 
   return (
     <InterviewSidebarLayout>
@@ -16,7 +40,7 @@ export const AvailableTimesSidebar = () => {
         <div className="grid grid-cols-2 gap-3">
           <BoxButton
             className="w-full"
-            onClick={() => setCalendarMode('수동생성')}
+            onClick={onManualScheduleModeButtonClick}
             size="xlarge"
             variant="filledSecondary"
           >
