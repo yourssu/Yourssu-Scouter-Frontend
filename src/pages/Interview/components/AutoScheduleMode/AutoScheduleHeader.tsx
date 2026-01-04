d81b659397d71e34779a589e79b792c219dc70b3
fecapark <fecapark@gmail.com>
2026-01-04 18:11:14 +0900

    feat: 자동생성모드 > 헤더에 '시간표 다시 만들기' 버튼 추가

--- src/pages/Interview/components/AutoScheduleMode/AutoScheduleHeader.tsx
+++ src/pages/Interview/components/AutoScheduleMode/AutoScheduleHeader.tsx
@@ -1,5 +1,6 @@
 import { HiSparkles } from 'react-icons/hi2';
 
+import { AutoScheduleRefetchButton } from '@/pages/Interview/components/AutoScheduleMode/AutoScheduleRefetchButton';
 import { InterviewHeaderLayout } from '@/pages/Interview/components/InterviewHeaderLayout';
 import { ScheduleModeToggleButton } from '@/pages/Interview/components/ScheduleModeToggleButton';
 
@@ -23,7 +24,10 @@ export const AutoScheduleHeader = ({
           onNextWeek={onNextWeek}
           onPrevWeek={onPrevWeek}
         />
-        <ScheduleModeToggleButton />
+        <div className="flex items-center gap-2.5">
+          <AutoScheduleRefetchButton />
+          <ScheduleModeToggleButton />
+        </div>
       </InterviewHeaderLayout.Row>
       <InterviewHeaderLayout.Row>
         <div className="bg-bg-brandSecondary text-text-brandPrimary typo-b3_rg_14 flex w-full max-w-90 items-center gap-2 rounded-md px-2.5 py-2">
