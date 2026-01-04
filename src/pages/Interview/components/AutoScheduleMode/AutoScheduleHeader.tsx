3cff7cd780d815b87f605bce3c0caee02b08c7e1
fecapark <fecapark@gmail.com>
2026-01-04 18:04:56 +0900

    feat: 자동생성모드 > 헤더에 AI 생성 관련 콜아웃 구현

--- src/pages/Interview/components/AutoScheduleMode/AutoScheduleHeader.tsx
+++ src/pages/Interview/components/AutoScheduleMode/AutoScheduleHeader.tsx
@@ -1,3 +1,5 @@
+import { HiSparkles } from 'react-icons/hi2';
+
 import { InterviewHeaderLayout } from '@/pages/Interview/components/InterviewHeaderLayout';
 import { ScheduleModeToggleButton } from '@/pages/Interview/components/ScheduleModeToggleButton';
 
@@ -22,6 +24,12 @@ export const AutoScheduleHeader = ({
           onPrevWeek={onPrevWeek}
         />
         <ScheduleModeToggleButton />
+      </InterviewHeaderLayout.Row>
+      <InterviewHeaderLayout.Row>
+        <div className="bg-bg-brandSecondary text-text-brandPrimary typo-b3_rg_14 flex w-full max-w-90 items-center gap-2 rounded-md px-2.5 py-2">
+          <HiSparkles />
+          AI가 자동생성한 시간표예요.
+        </div>
       </InterviewHeaderLayout.Row>
     </InterviewHeaderLayout>
   );
