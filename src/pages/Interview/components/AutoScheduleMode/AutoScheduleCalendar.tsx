23186912548509dfc97623e776b9389dcb4c69a4
Sanghyeok Park <fecapark@gmail.com>
2026-01-06 00:02:27 +0900

    feat: 자동생성모드에서 선택한 일정으로 캘린더를 렌더링해요 (#86)
    * fix: 인터뷰 페이지 레이아웃 > 사이드바 확장 방식이 캘린더 영역을 가지지 못하던 문제 수정
    
    * feat: 자동생성모드 > 선택한 일정으로 캘린더 렌더링

---
+++ src/pages/Interview/components/AutoScheduleMode/AutoScheduleCalendar.tsx
@@ -0,0 +1,47 @@
+import { setHours, setMinutes } from 'date-fns';
+
+import { useDateMap } from '@/hooks/useDateMap';
+import { AutoScheduleBlock } from '@/pages/Interview/components/AutoScheduleMode/AutoScheduleBlock';
+import { AutoScheduleCandidate } from '@/pages/Interview/components/AutoScheduleMode/type';
+import { InterviewCalendar } from '@/pages/Interview/components/InterviewCalendar/InterviewCalendar';
+import { useInterviewAutoScheduleContext } from '@/pages/Interview/context';
+
+interface AutoScheduleCalendarProps {
+  month: number;
+  scheduleCandidate: AutoScheduleCandidate;
+  week: number;
+  year: number;
+}
+
+export const AutoScheduleCalendar = ({
+  scheduleCandidate,
+  month,
+  week,
+  year,
+}: AutoScheduleCalendarProps) => {
+  const { duration } = useInterviewAutoScheduleContext();
+
+  const [map] = useDateMap({
+    initialEntries: scheduleCandidate.schedules.map((v) => [new Date(v.startTime), v]),
+    precision: duration === '1시간' ? '시간' : '분',
+  });
+
+  return (
+    <InterviewCalendar month={month} week={week} year={year}>
+      {({ date, hour, minute }) => {
+        const targetDate = setMinutes(setHours(date, hour), minute);
+        const schedule = map.get(targetDate);
+
+        return (
+          !!schedule && (
+            <AutoScheduleBlock
+              date={targetDate}
+              isFirstBlock={duration === '30분' || (duration === '1시간' && minute === 0)}
+              schedule={schedule}
+            />
+          )
+        );
+      }}
+    </InterviewCalendar>
+  );
+};
--- src/pages/Interview/components/InterviewPageLayout.tsx
+++ src/pages/Interview/components/InterviewPageLayout.tsx
@@ -23,7 +23,7 @@ export const InterviewPageLayout = ({
         <div className="flex w-full flex-[1_1_0]">
           <div className="border-line-basicMedium flex w-full flex-col border-t pt-2">
             <div className="border-line-basicMedium border-b pr-6">{header}</div>
-            <div className="flex flex-col pt-12 pr-6">{calendar}</div>
+            <div className="flex flex-[1_1_0] flex-col pt-12 pr-6">{calendar}</div>
           </div>
           <div className="border-line-basicMedium w-100 min-w-100 border-t border-l">{sidebar}</div>
         </div>
