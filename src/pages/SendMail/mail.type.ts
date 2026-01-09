a92cbefad1a5debf2fc595c9cd17aeb0c5a266a3
wwwlnyy <wwwlnyy@gmail.com>
2026-01-10 00:21:32 +0900

    refactor: 타입 네이밍 개선(HeaderType > EditorType)

--- src/pages/SendMail/mail.type.ts
+++ src/pages/SendMail/mail.type.ts
@@ -1,6 +1,7 @@
-export const HEADER_TYPES = ['normal', 'tabs'] as const;
-export type HeaderType = (typeof HEADER_TYPES)[number];
-export type EditorType = (typeof HEADER_TYPES)[number];
+// 메일 에디터 타입
+// normal: 일반 에디터, tabs: 수신자별 탭이 나눠진 에디터
+export const EDITOR_TYPES = ['normal', 'tabs'] as const;
+export type EditorType = (typeof EDITOR_TYPES)[number];
 
 export const RECIPIENT_IDS = ['recipient-0', 'recipient-1', 'recipient-2'] as const;
 
