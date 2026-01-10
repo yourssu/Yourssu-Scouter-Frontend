888251cb48aebee07afd939e2e9ac247ec470fcd
이나영 <112078717+wwwlnyy@users.noreply.github.com>
2026-01-10 16:51:04 +0900

    Revert "feat: 메일 페이지의 레이아웃을 공통화하고 상태 관리 컨텍스트를 만들었어요 (#89)"
    This reverts commit 56dc5a7677b601b1a396953762fb699b96b1f4df.

--- src/pages/SendMail/SendMail.tsx
+++ src/pages/SendMail/SendMail.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 
 import { InputField } from '@/components/InputField/InputField';
-import { MailDropdownSection } from '@/pages/SendMail/MailDropdownSection/MailDropdownSection';
+import { PageLayout } from '@/components/layouts/PageLayout';
+import { PartDropdown, TemplateDropdown } from '@/components/MailDropdown/MailDropdown';
 import { MailEditor } from '@/pages/SendMail/MailEditor/MailEditor';
-import { SendMailModeProvider } from '@/pages/SendMail/SendMailMode/SendMailMode';
-import { SendMailPageLayout } from '@/pages/SendMail/SendMailPageLayout/SendMailPageLayout';
 import { Part } from '@/query/part/schema';
 
 export const SendMail = () => {
@@ -12,29 +11,34 @@ export const SendMail = () => {
   const [selectedTemplateId, setSelectedTemplateId] = useState<number | undefined>(undefined);
 
   return (
-    <SendMailModeProvider>
-      <SendMailPageLayout
-        slots={{
-          dropdown: (
-            <MailDropdownSection
+    <PageLayout>
+      <div className="flex h-full flex-row">
+        <div className="flex w-full flex-col gap-[20px] p-[40px]">
+          <div className="flex w-full flex-row gap-[12px]">
+            <PartDropdown
+              disabled={selectedTemplateId !== undefined}
+              onSelectPart={setSelectedPart}
               selectedPart={selectedPart}
-              selectedTemplateId={selectedTemplateId}
-              setSelectedPart={setSelectedPart}
-              setSelectedTemplateId={setSelectedTemplateId}
             />
-          ),
-          info: (
+            <TemplateDropdown
+              disabled={selectedPart === undefined}
+              onSelectTemplateId={setSelectedTemplateId}
+              selectedTemplateId={selectedTemplateId}
+            />
+          </div>
+          <div className="flex h-full w-full flex-col gap-[20px]">
             <div className="gap-0">
               <InputField label="보내는 사람" />
               <InputField label="받는 사람" />
               <InputField label="숨은 참조" />
               <InputField label="제목" />
             </div>
-          ),
-          editor: <MailEditor type="normal" />,
-          sidebar: <div>Sidebar Content</div>,
-        }}
-      />
-    </SendMailModeProvider>
+            <div className="flex h-full w-full">
+              <MailEditor type="normal" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </PageLayout>
   );
 };
