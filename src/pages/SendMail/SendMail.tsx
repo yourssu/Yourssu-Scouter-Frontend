31a2ff3354689389fccaeeff9c65c6868e94818f
wwwlnyy <wwwlnyy@gmail.com>
2026-01-10 11:56:34 +0900

    refactor: SendMail 페이지 레이아웃 공통화

--- src/pages/SendMail/SendMail.tsx
+++ src/pages/SendMail/SendMail.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 import { InputField } from '@/components/InputField/InputField';
-import { PageLayout } from '@/components/layouts/PageLayout';
 import { MailDropdownSection } from '@/pages/SendMail/MailDropdownSection/MailDropdownSection';
 import { MailEditor } from '@/pages/SendMail/MailEditor/MailEditor';
+import { SendMailPageLayout } from '@/pages/SendMail/SendMailPageLayout/SendMailPageLayout';
 import { Part } from '@/query/part/schema';
 
 export const SendMail = () => {
@@ -11,28 +11,27 @@ export const SendMail = () => {
   const [selectedTemplateId, setSelectedTemplateId] = useState<number | undefined>(undefined);
 
   return (
-    <PageLayout>
-      <div className="flex h-full flex-row">
-        <div className="flex w-full flex-col gap-[20px] p-[40px]">
+    <SendMailPageLayout
+      slots={{
+        dropdown: (
           <MailDropdownSection
             selectedPart={selectedPart}
             selectedTemplateId={selectedTemplateId}
             setSelectedPart={setSelectedPart}
             setSelectedTemplateId={setSelectedTemplateId}
           />
-          <div className="flex h-full w-full flex-col gap-[20px]">
-            <div className="gap-0">
-              <InputField label="보내는 사람" />
-              <InputField label="받는 사람" />
-              <InputField label="숨은 참조" />
-              <InputField label="제목" />
-            </div>
-            <div className="flex h-full w-full">
-              <MailEditor type="normal" />
-            </div>
+        ),
+        info: (
+          <div className="gap-0">
+            <InputField label="보내는 사람" />
+            <InputField label="받는 사람" />
+            <InputField label="숨은 참조" />
+            <InputField label="제목" />
           </div>
-        </div>
-      </div>
-    </PageLayout>
+        ),
+        editor: <MailEditor type="normal" />,
+        sidebar: <div>Sidebar Content</div>,
+      }}
+    />
   );
 };
---
+++ src/pages/SendMail/SendMailPageLayout/SendMailPageLayout.tsx
@@ -0,0 +1,27 @@
+import { PageLayout } from '@/components/layouts/PageLayout';
+
+interface SendMailPageLayoutProps {
+  slots: {
+    dropdown: React.ReactNode;
+    editor: React.ReactNode;
+    info: React.ReactNode;
+    sidebar: React.ReactNode;
+  };
+}
+
+export const SendMailPageLayout = ({ slots }: SendMailPageLayoutProps) => {
+  return (
+    <PageLayout>
+      <div className="flex h-full flex-row">
+        <div className="flex h-full w-full flex-col gap-[20px] p-[40px]">
+          {slots.dropdown}
+          {slots.info}
+          <div className="flex h-full">{slots.editor}</div>
+        </div>
+        <div className="border-line-basicMedium w-100 min-w-100 border-t border-l">
+          {slots.sidebar}
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
