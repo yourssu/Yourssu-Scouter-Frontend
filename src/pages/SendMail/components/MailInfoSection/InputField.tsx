5e32bb419279e76a1db9df928f4778caebac796d
wwwlnyy <wwwlnyy@gmail.com>
2026-01-10 17:36:35 +0900

    feat: InputField 내부에 칩이 들어가도록 구현

--- src/pages/SendMail/components/MailInfoSection/InputField.tsx
+++ src/pages/SendMail/components/MailInfoSection/InputField.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
+
+import { InputChipGroup } from '@/pages/SendMail/MailInfoSection/InputChipGroup';
+
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
 export const InputField = ({ label }: InputFieldProps) => {
+  const [inputValue, setInputValue] = useState('');
   return (
     <div className="border-line-basicMedium flex h-[56px] flex-row gap-[12px] border-b-1 px-[20px] py-[16px]">
-      <div className="typo-b1_sb_16 h-[24px] w-[72px]">{label}</div>
-      <input className="typo-b1_rg_16 flex-1 border-0" />
+      <div className="typo-b1_sb_16 h-[24px] min-w-[72px]">{label}</div>
+      <div className="flex w-full items-center gap-[8px]">
+        <InputChipGroup items={[]} />
+        <input
+          className="typo-b1_rg_16 text-text-basicPrimary flex-1 border-0 bg-transparent outline-none focus:ring-0"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+      </div>
     </div>
   );
 };
