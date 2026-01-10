ab4d7d439259d824486ceac8ccb9fbcc1edaad47
wwwlnyy <wwwlnyy@gmail.com>
2026-01-10 12:44:03 +0900

    feat: SendMailMode 컨텍스트 작성

--- src/pages/SendMail/SendMail.tsx
+++ src/pages/SendMail/SendMail.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { InputField } from '@/components/InputField/InputField';
 import { MailDropdownSection } from '@/pages/SendMail/MailDropdownSection/MailDropdownSection';
 import { MailEditor } from '@/pages/SendMail/MailEditor/MailEditor';
+import { SendMailModeProvider } from '@/pages/SendMail/SendMailMode/SendMailMode';
 import { SendMailPageLayout } from '@/pages/SendMail/SendMailPageLayout/SendMailPageLayout';
 import { Part } from '@/query/part/schema';
 
@@ -11,27 +12,29 @@ export const SendMail = () => {
   const [selectedTemplateId, setSelectedTemplateId] = useState<number | undefined>(undefined);
 
   return (
-    <SendMailPageLayout
-      slots={{
-        dropdown: (
-          <MailDropdownSection
-            selectedPart={selectedPart}
-            selectedTemplateId={selectedTemplateId}
-            setSelectedPart={setSelectedPart}
-            setSelectedTemplateId={setSelectedTemplateId}
-          />
-        ),
-        info: (
-          <div className="gap-0">
-            <InputField label="보내는 사람" />
-            <InputField label="받는 사람" />
-            <InputField label="숨은 참조" />
-            <InputField label="제목" />
-          </div>
-        ),
-        editor: <MailEditor type="normal" />,
-        sidebar: <div>Sidebar Content</div>,
-      }}
-    />
+    <SendMailModeProvider>
+      <SendMailPageLayout
+        slots={{
+          dropdown: (
+            <MailDropdownSection
+              selectedPart={selectedPart}
+              selectedTemplateId={selectedTemplateId}
+              setSelectedPart={setSelectedPart}
+              setSelectedTemplateId={setSelectedTemplateId}
+            />
+          ),
+          info: (
+            <div className="gap-0">
+              <InputField label="보내는 사람" />
+              <InputField label="받는 사람" />
+              <InputField label="숨은 참조" />
+              <InputField label="제목" />
+            </div>
+          ),
+          editor: <MailEditor type="normal" />,
+          sidebar: <div>Sidebar Content</div>,
+        }}
+      />
+    </SendMailModeProvider>
   );
 };
