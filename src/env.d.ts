3bfa30e6b6de21fa1c4b58714717d74b7756da6e
fecapark <fecapark@gmail.com>
2026-02-21 17:45:08 +0900

    feat: 환경별 config 분기 처리

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
-  readonly VITE_USE_POLLING: string;
+  readonly VITE_S3_BASE_URL: string;
+  readonly VITE_STAGE: 'dev' | 'prod';
 }
 
 interface ImportMeta {
