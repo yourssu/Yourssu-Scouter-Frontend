daee89b97ea1ddfe280db37156332a47e4c9ca72
fecapark <fecapark@gmail.com>
2026-02-21 22:14:10 +0900

    feat: 장소 겹침 UI 구현

--- src/pages/Interview/components/InterviewScheduleMode/InterviewScheduleSidebar.tsx
+++ src/pages/Interview/components/InterviewScheduleMode/InterviewScheduleSidebar.tsx
@@ -13,6 +13,20 @@ export const InterviewScheduleSidebar = ({ schedules }: InterviewScheduleSidebar
 
   return (
     <InterviewSidebarLayout>
+      {confilctGroups.length > 0 && (
+        <div className="bg-bg-basicDefault border-line-basicMedium flex w-full shrink-0 flex-col items-start gap-1 border-b px-4 py-3">
+          <div className="typo-t4_sb_18 flex items-center">
+            <span className="text-text-basicSecondary">장소 겹침</span>
+            <span className="text-text-brandSecondary whitespace-pre">
+              {' '}
+              {confilctGroups.length}건
+            </span>
+          </div>
+          <p className="typo-b2_rg_15 text-text-basicTertiary truncate">
+            HR 매니저는 겹치는 장소를 변경해주세요.
+          </p>
+        </div>
+      )}
       <InterviewSidebarLayout.CardList>
         {confilctGroups.map((group, i) => (
           <InterviewSidebarConflictCard key={i} schedules={group} />
--- src/pages/Interview/components/InterviewScheduleMode/InterviewSidebarConflictCard.tsx
+++ src/pages/Interview/components/InterviewScheduleMode/InterviewSidebarConflictCard.tsx
@@ -1,7 +1,6 @@
-import { IcAlertTriangleFilled } from '@yourssu/design-system-react';
-import { Fragment as ReactFragment } from 'react/jsx-runtime';
+import { IcArrowsChevronDownLine, IcClockFilled } from '@yourssu/design-system-react';
+import { differenceInMinutes } from 'date-fns';
 
-import { InterviewSidebarCard } from '@/pages/Interview/components/InterviewSidebarCard';
 import { Schedule } from '@/query/schedule/schema';
 import { formatTemplates } from '@/utils/date';
 
@@ -9,34 +8,64 @@ interface InterviewSidebarConflictCardProps {
   schedules: Schedule[];
 }
 
+function formatDuration(startTime: string, endTime: string) {
+  const diff = differenceInMinutes(endTime, startTime);
+  if (diff >= 60) {
+    const hours = Math.floor(diff / 60);
+    const mins = diff % 60;
+    return `${hours}시간${mins > 0 ? ` ${mins}분` : ''}`;
+  }
+  return `${diff}분`;
+}
+
 export const InterviewSidebarConflictCard = ({ schedules }: InterviewSidebarConflictCardProps) => {
+  if (!schedules.length) {
+    return null;
+  }
+
+  const dateStr = formatTemplates['01/01(월)'](schedules[0].startTime);
+
   return (
-    <InterviewSidebarCard>
-      <InterviewSidebarCard.Title
-        leftIcon={<IcAlertTriangleFilled className="text-icon-brandPrimary size-full" />}
-      >
-        겹치는 일정 주의
-      </InterviewSidebarCard.Title>
-      <InterviewSidebarCard.Content>
-        <div className="grid grid-cols-[auto_1fr] items-center gap-x-2 gap-y-3">
-          {schedules.map((schedule) => (
-            <ReactFragment key={schedule.id}>
-              <div className="typo-b3_sb_14">{schedule.part}</div>
-              <div className="flex w-full items-center gap-2">
-                <div className="typo-b2_rg_15 bg-bg-basicLight rounded-full px-3 py-2">
-                  {schedule.name}
+    <div className="bg-bg-basicDefault border-line-basicMedium flex w-full flex-col gap-4 rounded-[14px] border p-4">
+      <div className="flex w-full items-center justify-between">
+        <p className="typo-b1_sb_16 text-text-basicSecondary truncate">{dateStr}</p>
+      </div>
+
+      <div className="flex w-full flex-col items-center gap-5">
+        {schedules.map((schedule) => (
+          <div className="flex w-full flex-col items-start gap-2" key={schedule.id}>
+            <div className="flex w-full items-center justify-between">
+              <div className="flex items-center gap-2">
+                <div className="bg-bg-basicLight flex h-8 shrink-0 items-center justify-center rounded-[40px] px-3">
+                  <span className="typo-b3_rg_14 text-text-basicSecondary text-center">
+                    {schedule.name}
+                  </span>
                 </div>
-                <div className="typo-b1_rg_16 bg-bg-basicLight flex-1 rounded-xl p-3">
-                  {formatTemplates['01/01(월) 00:00'](schedule.startTime)}
+                <p className="typo-b3_sb_14 text-text-basicTertiary truncate">{schedule.part}</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <p className="typo-b2_rg_15 text-text-basicTertiary truncate">
+                  {formatTemplates['23:59'](schedule.startTime)}~
+                  {formatTemplates['23:59'](schedule.endTime)}
+                </p>
+                <div className="bg-line-basicMedium h-[14px] w-px shrink-0" />
+                <div className="flex shrink-0 items-center gap-1">
+                  <IcClockFilled className="text-text-basicSecondary size-4" />
+                  <p className="typo-b3_sb_14 text-text-basicSecondary">
+                    {formatDuration(schedule.startTime, schedule.endTime)}
+                  </p>
                 </div>
               </div>
-            </ReactFragment>
-          ))}
-        </div>
-      </InterviewSidebarCard.Content>
-      <InterviewSidebarCard.Warning left="유어슈 동아리방">
-        사용이 겹쳐요! 다른 장소를 예약해주세요!
-      </InterviewSidebarCard.Warning>
-    </InterviewSidebarCard>
+            </div>
+            <div className="bg-bg-basicDefault border-line-basicMedium flex h-12 w-full shrink-0 items-center gap-1 rounded-xl border px-4">
+              <p className="typo-b1_sb_16 text-text-basicPrimary flex-1 whitespace-pre-wrap">
+                동아리방
+              </p>
+              <IcArrowsChevronDownLine className="size-5" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
