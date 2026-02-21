7d79cc76f44ddf8b21b97ce7f68fbd9938bab56a
fecapark <fecapark@gmail.com>
2026-02-21 20:57:26 +0900

    feat: 면접 방식 셀렉트 구현

--- src/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebar.tsx
+++ src/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebar.tsx
@@ -1,6 +1,7 @@
 import { InterviewSidebarLayout } from '@/pages/Interview/components/InterviewSidebarLayout';
 import { ManualScheduleSaveButton } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSaveButton';
 import { ManualScheduleSidebarDurationCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarDurationCard';
+import { ManualScheduleSidebarMethodCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarMethodCard';
 import { ManualScheduleSidebarPartCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarPartCard';
 import { ManualScheduleSidebarProgressCard } from '@/pages/Interview/components/ManualScheduleMode/ManualScheduleSidebarProgressCard';
 import { useInterviewPartSelectionContext } from '@/pages/Interview/context';
@@ -8,11 +9,15 @@ import { Applicant } from '@/query/applicant/schema';
 
 interface ManualScheduleSidebarProps {
   completedApplicants: [Date, Applicant][];
+  method: '대면' | '비대면';
+  onChangeMethod: (method: '대면' | '비대면') => void;
   totalApplicantCount: number;
 }
 
 export const ManualScheduleSidebar = ({
   completedApplicants,
+  method,
+  onChangeMethod,
   totalApplicantCount,
 }: ManualScheduleSidebarProps) => {
   const { partId } = useInterviewPartSelectionContext();
@@ -20,6 +25,7 @@ export const ManualScheduleSidebar = ({
     <InterviewSidebarLayout>
       <InterviewSidebarLayout.CardList>
         <ManualScheduleSidebarPartCard />
+        <ManualScheduleSidebarMethodCard method={method} onChangeMethod={onChangeMethod} />
         <ManualScheduleSidebarDurationCard />
         {partId && (
           <ManualScheduleSidebarProgressCard
