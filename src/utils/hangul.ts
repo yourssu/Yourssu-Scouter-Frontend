095ba1e0f196ae23b08ec8e2afb13b7d73f0b4cd
wwwlnyy <wwwlnyy@gmail.com>
2026-02-23 01:05:08 +0900

    feat: 파트 선택 여부에 따라 지원자 다이얼로그 다르게 뜨게 변경

---
+++ src/utils/hangul.ts
@@ -0,0 +1,24 @@
+// 초성 검색
+const CHO = [
+  'ㄱ',
+  'ㄲ',
+  'ㄴ',
+  'ㄷ',
+  'ㄸ',
+  'ㄹ',
+  'ㅁ',
+  'ㅂ',
+  'ㅃ',
+  'ㅅ',
+  'ㅆ',
+  'ㅇ',
+  'ㅈ',
+  'ㅉ',
+  'ㅊ',
+  'ㅋ',
+  'ㅌ',
+  'ㅍ',
+  'ㅎ',
+];
+export const getChosung = (str: string) =>
+  str.replace(/[가-힣]/g, (c) => CHO[Math.floor((c.charCodeAt(0) - 44032) / 588)]);
